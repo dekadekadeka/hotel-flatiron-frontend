45e78b3d4ebfd3c48a793dc88ac3ab710411bd98
github username <dekadekadeka@gmail.com>
2019-07-06 17:02:35 -0400

    added reservation pages

---
+++ src/pages/MyReservation.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MyReservation extends Component {
+    render() {
+        return (
+            <div>
+                This is where the user's personal info will be
+            </div>
+        )
+    }
+}
