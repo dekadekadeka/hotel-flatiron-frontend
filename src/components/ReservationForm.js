8d4d9ce66307714633199daef70dc84b1675517f
github username <dekadekadeka@gmail.com>
2019-07-07 20:57:45 -0400

    tried to do form

--- src/components/ReservationForm.js
+++ src/components/ReservationForm.js
@@ -3,26 +3,40 @@ import Calendar from "react-calendar"
 import Moment from "react-moment"
 
 export default class ReservationForm extends Component {
+    
+
     state={
         // name: "",
-        dates: new Date()
-        // guests: ""
+        dates: new Date(),
+        guests: ""
     }
 
-    onChange = (dates) => this.setState({ dates })
-
-
+    onChange = (dates) => this.setState({ 
+        dates,
+        guests: this.state.guests})
     render() {
         let datesArray = this.state.dates;
+
         return (
-            <div>
-                The reservation form will go here
+        <div className="res-form">
+        <div className="calendar">
                 <Calendar selectRange={true}
                 onChange={this.onChange}
                 value={this.state.dates}/>
-                <p>Check-in: <Moment format="MM/DD/YYYY">{datesArray[0]}</Moment></p>
-                <p>Check-out: <Moment format="MM/DD/YYYY">{datesArray[1]}</Moment></p>
             </div>
+        <div className="res-info">
+            <h6>Check-in: <Moment format="MM/DD/YYYY">{datesArray[0]}</Moment></h6>
+            <h6>Check-out: <Moment format="MM/DD/YYYY">{datesArray[1]}</Moment></h6>
+            <h6>Guests:</h6>
+            <select name="capacity" 
+            id="capacity"
+            value={this.props.info.capacity}
+            className="form-control"
+            onChange={this.onChange}>
+                <option value="capacity">{this.props.info.capacity}</option>
+            </select>
+        </div>
+        </div>
         )
     }
 }
