90c0c981d33fbdfaaed1a562c1700c03cf29fbef
Preston Elliott <preston.elliott24@gmail.com>
2019-07-09 13:01:27 -0400

    add new user form

--- src/components/CreateAccount.js
+++ src/components/CreateAccount.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react'
-import CreateAccount from "./CreateAccount"
+import Authenticate from "./Authenticate"
 
-export default class Authenticate extends Component {
+export default class CreateAccount extends Component {
     constructor(){
         super()
         this.state={
+            firstName: "",
+            lastName: "",
             username: "",
-            password: "",
-            isOpen: false
+            password: ""
         }
-}
+    }
+
+    
 
     changeHandler = (e) => {
         console.log(e.target.value)
@@ -42,35 +45,40 @@ export default class Authenticate extends Component {
         })
     }
 
-    handleOpen = () => {
-        console.log("handle open")
-        this.setState({isOpen: !this.state.isOpen})
-    }
-
-
     render() {
+        
         return (
-            <>
-                <form className="auth-form" onSubmit={this.login}>
+            <div>
+                <form className="create-form" onSubmit={this.login}>
+                    <label htmlFor="firstname">First Name: </label>
+                    <input type="firstname" 
+                    name="firstname"   
+                    value={this.state.firstName} 
+                    onChange={this.changeHandler} />
+
+                    <label htmlFor="lastname">Last Name: </label>
+                    <input type="lastname" 
+                    name="lastname"   
+                    value={this.state.lastName} 
+                    onChange={this.changeHandler} />
+
                     <label htmlFor="username">Username: </label>
                     <input type="username" 
                     name="username"   
                     value={this.state.username} 
                     onChange={this.changeHandler} />
+
                     <label htmlFor="password">Password: </label>
                     <input type="password" 
                     name="password"   
                     value={this.state.password} 
                     onChange={this.changeHandler} />
+
                     <input type="submit" />
                 </form>
-                <h4>Not a member yet?</h4>
-                <button className="btn-primary" onClick={this.handleOpen}>Sign up now!</button>
-                {this.state.isOpen && <CreateAccount />}
-                {/* Sign up now will be a link to sign-in page 
-                if we use authentication */}
-            </>
+            </div>
         )
     }
-}
 
+}
+    
