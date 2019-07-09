eab25735eeca9b4b7bf736c6da0f15014b04fd35
github username <github email address>
2019-07-09 10:50:39 -0400

    fixed sessions controller

--- src/components/Authenticate.js
+++ src/components/Authenticate.js
@@ -9,20 +9,42 @@ export default class Authenticate extends Component {
         }
 }
 
-changeHandler = (e) => {
-    console.log(e.target.value)
-    const name = e.target.name
-    const value = e.target.value
+    changeHandler = (e) => {
+        console.log(e.target.value)
+        const name = e.target.name
+        const value = e.target.value
 
-    this.setState({
-        [name]: value
-    })
-
+        this.setState({
+            [name]: value
+        })
     }
+
+    login = e => {
+        e.preventDefault()
+        fetch('http://localhost:3000/login', {
+            method: 'POST',
+            headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                user:{
+                    username: e.target.username.value,
+                    password: e.target.password.value
+                }
+            })
+        })
+        .then(res => res.json())
+        .then(res => {
+            if(res.error || res.errors)
+                console.log(res)
+            else
+                localStorage.setItem('jwt', res.jwt)
+        })
+    }
+
+
     render() {
         return (
             <>
-                <form className="auth-form">
+                <form className="auth-form" onSubmit={this.login}>
                     <label htmlFor="username">Username: </label>
                     <input type="username" 
                     name="username"   
@@ -33,6 +55,7 @@ changeHandler = (e) => {
                     name="password"   
                     value={this.state.password} 
                     onChange={this.changeHandler} />
+                    <input type="submit" />
                 </form>
                 <h4>Not a member yet?</h4>
                 <h4>Sign up now!</h4>
